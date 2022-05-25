c32dc062c9b5066bc253a836617b04e6e2c7a1c5
Dixiang Yuan <yuan.di@northeastern.edu>
2022-05-25 12:39:13 -0400

    Put 2*3 Grid in the new activity

--- app/src/main/java/edu/neu/madcourse/numad22su_dixiangyuan/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22su_dixiangyuan/MainActivity.java
@@ -9,15 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     Button aboutMe;
     Button clicky;
-    Button showTextA;
-    Button showTextB;
-    Button showTextC;
-    Button showTextD;
-    Button showTextE;
-    Button showTextF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,48 +36,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
-        showTextA = findViewById(R.id.buttonA);
-        showTextA.setOnClickListener(this);
-        showTextB = findViewById(R.id.buttonB);
-        showTextB.setOnClickListener(this);
-        showTextC = findViewById(R.id.buttonC);
-        showTextC.setOnClickListener(this);
-        showTextD = findViewById(R.id.buttonD);
-        showTextD.setOnClickListener(this);
-        showTextE = findViewById(R.id.buttonE);
-        showTextE.setOnClickListener(this);
-        showTextF = findViewById(R.id.buttonF);
-        showTextF.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onClick(View v) {
-        TextView showText = findViewById(R.id.textViewForGrid);
-        switch (v.getId()) {
-            case R.id.buttonA:
-                showText.setText("Pressed: A ");
-                break;
-            case R.id.buttonB:
-                showText.setText("Pressed: B ");
-                break;
-
-            case R.id.buttonC:
-                showText.setText("Pressed: C ");
-                break;
-
-            case R.id.buttonD:
-                showText.setText("Pressed: D ");
-                break;
-
-            case R.id.buttonE:
-                showText.setText("Pressed: E ");
-                break;
-
-            case R.id.buttonF:
-                showText.setText("Pressed: F ");
-                break;
-
-        }
     }
 }
